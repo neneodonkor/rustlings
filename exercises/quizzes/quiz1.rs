7913082fda38cbd2be6accd09058ccabeb416cc2
Nene Odonkor <38510646+neneodonkor@users.noreply.github.com>
2026-02-19 00:50:48 +0000

    First commit

---
+++ exercises/quizzes/quiz1.rs
@@ -0,0 +1,44 @@
+// This is a quiz for the following sections:
+// - Variables
+// - Functions
+// - If
+//
+// Mary is buying apples. The price of an apple is calculated as follows:
+// - An apple costs 2 rustbucks.
+// - However, if Mary buys more than 40 apples, the price of each apple in the
+// entire order is reduced to only 1 rustbuck!
+
+// TODO: Write a function that calculates the price of an order of apples given
+// the quantity bought.
+// fn calculate_price_of_apples(???) -> ??? { ??? }
+
+fn main() {
+    // You can optionally experiment here.
+    let result = calculate_price_of_apples(90);
+    println!("The total price is {result}.")
+}
+
+fn calculate_price_of_apples(number: i64) -> i64 {
+    const STANDARD_PRICE: i64 = 2;
+    const DISCOUNTED_PRICE: i64 = 1;
+
+    if number <= 40 {
+        return number * STANDARD_PRICE;
+    } else {
+        return number * DISCOUNTED_PRICE;
+    }
+}
+
+// Don't change the tests!
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn verify_test() {
+        assert_eq!(calculate_price_of_apples(35), 70);
+        assert_eq!(calculate_price_of_apples(40), 80);
+        assert_eq!(calculate_price_of_apples(41), 41);
+        assert_eq!(calculate_price_of_apples(65), 65);
+    }
+}
